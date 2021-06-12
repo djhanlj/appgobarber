e97a75cf5d64b2f75e70dad1158e0910e785925e
Djair Soares <djhanlj@gmail.com>
2021-06-12 18:02:16 -0300

    configuração de da lib react-vector-icons, componentes de button e input

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Container, Title } from './styles';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import logoImg from '../../assets/logo.png';
+import { Container, Title } from './styles';
 
 const SigIn: React.FC = () => {
 
@@ -10,6 +13,9 @@ const SigIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => console.log('Deu')}>Entrar</Button>
     </Container>
   );
 }
